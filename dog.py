c6e4b5bb470f5bed1235a473b1fabf87e50c8bfd
mxrtydluffy <marty.marinda@students.dominican.edu>
2022-11-14 22:15:19 -0800

    created dog class

--- dog.py
+++ dog.py
@@ -1,11 +1,12 @@
 # dog.py
 class Dog:
-  def __init__(self, name):
+  # Required properties are defined inside the __init__ constructor method.
+  def __init__(self, name, breed):
     self.name = name
+    self.breed = breed
     print("dog initialized!")
 
-# the same instantiation call that creates a Dog object,
-# but now we've assigned it to the value of the my_dog variable
-my_dog = Dog("Rex")
-print(my_dog)
-print(my_dog.name)
+  # Methods are defined as their own named functions inside the class.
+  # Need to put the "self" parameter every time we make a class method.
+  def bark(self):
+    print("Woof!")
