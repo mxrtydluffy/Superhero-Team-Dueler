f2344ef2cfa10ec1fe161299f0761b04a3aa72f2
Marty <mxrty.marinda21@gmail.com>
2022-11-18 21:59:37 -0800

    implemented team attack and defense

--- team.py
+++ team.py
@@ -22,3 +22,38 @@ class Team:
 
     def add_hero(self, hero):
         self.heroes.append(hero)
+
+    def stats(self):
+        for hero in self.heroes:
+            kd = hero.kills / hero.deaths
+            print(f"{hero.name} Kill/Deaths: {kd}")
+    
+    # current_health = starting_health
+    def revive_heroes(self, health = 100):
+        for hero in self.heroes:
+            hero.current_health = hero.starting_health
+
+    # Attacking each team
+    def attack(self, other_team):
+        living_heroes = list()
+        living_opponents = list()
+
+        for hero in self.heroes:
+            living_heroes.append(hero)
+
+        for hero in other_team.heroes:
+            living_opponents.append(hero)
+
+        while len(living_heroes) > 0 and len(living_opponents) > 0:
+            # Selects random hero for each team
+            hero = random.choice(living_heroes)
+            opponent = random.choice(living_opponents)
+            # Heros on both sides fight each other
+            hero.fight(opponent) 
+            
+            # Updates list of living_heroes and living_opponents
+            # Then reflects its data
+            if hero.is_alive() == True:
+                living_opponents.remove(opponent)
+            else:
+                living_heroes.remove(hero)
