987d9e34cdd5a0840698a14a61f2f32d67789a6a
Marty <mxrty.marinda21@gmail.com>
2022-11-18 23:12:11 -0800

    Added Battle Arena

--- weapon.py
+++ weapon.py
@@ -4,6 +4,6 @@ import random
 
 class Weapon(Ability):
     def attack(self):
-        min_damange = self.max_damage // 2
+        min_damage = self.max_damage // 2
         random_value = random.randint(self.max_damage // 2, self.max_damage)
         return random_value
