7cdc5d2bb1dec9635e6128a8e963a98c67166390
Marty <mxrty.marinda21@gmail.com>
2022-11-18 20:49:23 -0800

    implemented inheritance

--- weapon.py
+++ weapon.py
@@ -5,5 +5,5 @@ import random
 class Weapon(Ability):
     def attack(self):
         min_damange = self.max_damage // 2
-        random_value = random.randint(min_damange, self.max_damage)
+        random_value = random.randint(self.max_damage // 2, self.max_damage)
         return random_value
